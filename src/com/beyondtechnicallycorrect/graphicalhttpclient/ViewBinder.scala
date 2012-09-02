0c1818d1520e835a56105a34761ee4d11ec486bb
Jeffrey Charles <jeffreycharles@gmail.com>
2012-09-02 17:16:40 -0400

    Refactor ViewBinder

--- src/com/beyondtechnicallycorrect/graphicalhttpclient/ViewBinder.scala
+++ src/com/beyondtechnicallycorrect/graphicalhttpclient/ViewBinder.scala
@@ -6,82 +6,60 @@ import com.beyondtechnicallycorrect.graphicalhttpclient.bindings._
 
 object ViewBinder {
   
-  val url: InputField[URL] =
-    new InputField(
-        value = "",
-        enabled = true,
-        toUnderlying = input => {
-          try {
-            val url = new URL(input)
-            url.toURI() // performs additional validation
-            (true, url)
-          } catch {
-            case e: URISyntaxException => (false, null)
-            case e: MalformedURLException => (false, null)
-          }
-        },
-        signalUpdate = this.updateView
-      )
-  val headers: InputField[Iterable[(String, String)]] =
-    new InputField(
-        value = "",
-        enabled = true,
-        toUnderlying = input => {
-          val headerKeyValuePairs =
-            input
-              .split(sys.props("line.separator"))
-              .map(_.split(":").map(_.trim))
-          val valid = headerKeyValuePairs.forall(_.length == 2)
-          if(valid) {
-            (true, headerKeyValuePairs.map(pair => (pair(0), pair(1))))
-          } else {
-            (false, null)
-          }
-        },
-        signalUpdate = this.updateView
-      )
-  val requestBody: InputField[String] =
-    new InputField(
-        value = "",
-        enabled = true,
-        toUnderlying = input => (true, input),
-        signalUpdate = this.updateView
-      )
+  val url = createInputField[URL](
+      toUnderlying = input => {
+        try {
+          val url = new URL(input)
+          url.toURI() // performs additional validation
+          (true, url)
+        } catch {
+          case e: URISyntaxException => (false, null)
+          case e: MalformedURLException => (false, null)
+        }
+      }
+    )
+  val headers = createInputField[Iterable[(String, String)]](
+      toUnderlying = input => {
+        val headerKeyValuePairs =
+          input
+            .split(sys.props("line.separator"))
+            .map(_.split(":").map(_.trim))
+        val valid = headerKeyValuePairs.forall(_.length == 2)
+        if(valid) {
+          (true, headerKeyValuePairs.map(pair => (pair(0), pair(1))))
+        } else {
+          (false, null)
+        }
+      }
+    )
+  val requestBody = createInputField[String](
+      toUnderlying = input => (true, input)
+    )
   
-  val getButton =
-    new ButtonBinding(
-        enabled = true,
-        clicked = () => {},
-        signalUpdate = this.updateView
-      )
-  val postButton =
-    new ButtonBinding(
-        enabled = true,
-        clicked = () => {},
-        signalUpdate = this.updateView
-      )
-  val putButton =
-    new ButtonBinding(
-        enabled = true,
-        clicked = () => {},
-        signalUpdate = this.updateView
-      )
-  val deleteButton =
-    new ButtonBinding(
-        enabled = true,
-        clicked = () => {},
-        signalUpdate = this.updateView
-      )
-  val cancelButton =
-    new ButtonBinding(
-        enabled = false,
-        clicked = () => {},
-        signalUpdate = this.updateView
-      )
+  val getButton = createButton(clicked = () => {})
+  val postButton = createButton(clicked = () => {})
+  val putButton = createButton(clicked = () => {})
+  val deleteButton = createButton(clicked = () => {})
+  val cancelButton = createButton(enabled = false, clicked = () => {})
   
   val response = new OutputField(value = "", signalUpdate = this.updateView)
   
-  def updateView() {
+  private def updateView() {
     UserInterface.valueChanged()
   }
+  
+  private def createInputField[T <: AnyRef](toUnderlying: String => (Boolean, T)): InputField[T] =
+    new InputField(
+        value = "",
+        enabled = true,
+        toUnderlying = toUnderlying,
+        signalUpdate = this.updateView
+    )
+  
+  private def createButton(enabled: Boolean = true, clicked: () => Unit): ButtonBinding =
+    new ButtonBinding(
+        enabled = enabled,
+        clicked = clicked,
+        signalUpdate = this.updateView
+      )
 }
